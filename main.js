7fb63bd2004f067830cb0597f9386148c5a93f25
Tommy Gaessler <tommy.gaessler@gmail.com>
2016-10-03 22:02:59 -0600

    refactoring my janky af code from 8 months ago

---
+++ main.js
@@ -0,0 +1,8 @@
+function toggle_menu(id) {
+  var element = document.getElementById(id);
+  if(element.style.display == 'block') {
+    element.style.display = 'none';
+  } else {
+    element.style.display = 'block';
+  }
+}
