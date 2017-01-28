983f9405694eac9034c3b554d49d291dc819aa2d
Tommy Gaessler <tommy.gaessler@gmail.com>
2017-01-27 21:49:17 -0700

    added email error page

---
+++ emailerror.php
@@ -0,0 +1,12 @@
+<?php include('header.php');?>
+<main>
+  <div class="wrapper">
+
+    <div class="contentbox">
+
+      <h2>Email Failed to send.</h2>
+      <a href="contact.php" class="thank-you-link">Try Again?</a>
+    </div>
+  </div>
+</main>
+<?php include('footer.php');?>
